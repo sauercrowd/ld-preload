54ff1f9dfa40b461880c62d8fc505656a4122402
sauercrowd <sauercrowd@posteo.no>
2019-01-25 13:41:14 +0000

    print only non-print mallocs

--- preload.c
+++ preload.c
@@ -2,22 +2,32 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
 
 typedef void * (*orig_malloc_type)(size_t size);
 typedef void (*orig_free_type)(void *ptr);
 
 static size_t jonas_count = 0;
 
+static int just_malloc = 0;
+
 void *malloc(size_t size){
-//	printf("MALLOC [%d]", size);
-	jonas_count += size;
+	if(!just_malloc){
+		just_malloc = 1;
+		printf("MALLOC [%d]\n", size);
+		just_malloc = 0;
+	}else{
+		jonas_count += size;
+	}
 	orig_malloc_type orig_malloc;
 	orig_malloc = (orig_malloc_type)dlsym(RTLD_NEXT, "malloc");
 	return orig_malloc(size);
 }
 
 void free(void *ptr){
-	printf("[SIZE] %d\n", jonas_count);
+//	printf("[FREE] %d\n", jonas_count);
 	orig_free_type orig_free;
 	orig_free = (orig_free_type)dlsym(RTLD_NEXT, "free");
 	return orig_free(ptr);
